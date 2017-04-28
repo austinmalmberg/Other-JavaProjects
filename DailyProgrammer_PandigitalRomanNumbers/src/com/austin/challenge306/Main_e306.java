1d085447b743b397c9feca3538c411af2435a82c
Austin Malmberg <austin.malmberg@gmail.com>
2017-04-27 20:56:14 -0400

    Added description to DailyProgrammer_PandigitalRomanNumbers

--- DailyProgrammer_PandigitalRomanNumbers/src/com/austin/challenge306/Main_e306.java
+++ DailyProgrammer_PandigitalRomanNumbers/src/com/austin/challenge306/Main_e306.java
@@ -3,6 +3,26 @@ package com.austin.challenge306;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+/**
+ * Description
+ * 
+ * 1474 is a pandigital in Roman numerals (MCDLXXIV). It uses each of the symbols I, V, X, L, C, and M at least once.
+ * Your challenge today is to find the small handful of pandigital Roman numbers up to 2000.
+ * 
+ * Output Description
+ * 
+ * A list of numbers. Example:
+ * 1 (I), 2 (II), 3 (III), 8 (VIII) (Examples only, these are not pandigital Roman numbers)
+ * 
+ * Challenge Input
+ * Find all numbers that are pandigital in Roman numerals using each of the symbols I, V, X, L, C, D and M exactly once.
+ * 
+ * Challenge Input Solution
+ * 1444, 1446, 1464, 1466, 1644, 1646, 1664, 1666
+ * 
+ * @author Austin Malmberg
+ *
+ */
 public class Main_e306 {
 	public static void main(String[] args) {
 		PandigitalRomanNumbers prn = new PandigitalRomanNumbers(3999);
