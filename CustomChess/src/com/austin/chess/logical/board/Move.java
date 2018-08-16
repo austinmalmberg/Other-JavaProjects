cc3c64b6925c6a5915571a6ce18f39202c80a5b1
Austin Malmberg <austin.malmberg@gmail.com>
2018-08-15 22:09:12 -0400

    Updated variable references in CustomChess/.../Move.java

--- CustomChess/src/com/austin/chess/logical/board/Move.java
+++ CustomChess/src/com/austin/chess/logical/board/Move.java
@@ -1,4 +1,4 @@
-package com.austin.chess;
+package com.austin.chess.logical.board;
 
 import java.awt.Point;
 
@@ -6,30 +6,36 @@ public class Move {
 	
 	private final String FILES = "abcdefghijklmnopqrstuvwxyz".substring(0, 8);
 	
-	private String board;		// change to Board
 	private final Point from;
 	private final Point to;
 	
 	private final String movingPiece;
 	private final String capturedPiece;
+	
+	private final boolean check;
+	private final boolean checkmate;
 
-	public Move(String board, Point from, Point to, String movingPiece, String capturedPiece) {
-		this.board = board;
+	public Move(Point from, Point to, String movingPiece, String capturedPiece, boolean check, boolean checkmate) {
 		this.from = from;
 		this.to = to;
 		
 		this.movingPiece = movingPiece;
 		this.capturedPiece = capturedPiece;
+		
+		this.check = check;
+		this.checkmate = checkmate;
 	}
 	
 	public Point from() { return from; }
 	public Point to() { return to; }
 	
 	public String fromNotation() { return "" + FILES.charAt(from.y) + from.x; }
-	public String toNotation() { return "" + FILES.charAt(to.y) + to.x; }
-	
-	public boolean pieceCaptured() { return capturedPiece != null; }
+	public String toNotation() { return "" + FILES.charAt(to.y) + to.x; } 
 	
 	public String movingPiece() { return movingPiece; }
 	public String capturedPiece() { return capturedPiece; }
+	
+	public boolean pieceCaptured() { return capturedPiece != null; }
+	public boolean inCheck() { return check; }
+	public boolean checkMate() { return checkmate; }
 }
